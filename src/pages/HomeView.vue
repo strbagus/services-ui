f00694e844ebdd95f2168666fb4a64f9ac7b09b9
Satrio Bagus <satrio.baguswbw@gmail.com>
2025-10-28 13:09:30 +0700

    ench: homeview, doc title, cast date

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,5 +1,31 @@
 <script setup>
+import { Card } from 'primevue';
+import { RouterLink } from 'vue-router';
+
+const routes = [
+  {
+    title: "Homelab Monitoring",
+    path: "/homelab"
+  }
+]
 </script>
 <template>
-  <h1>Hello World</h1>
+  <div class="flex flex-col w-full min-h-dvh justify-center items-center">
+    <h1 class="text-3xl">
+      <i class="pi pi-sitemap" style="font-size: 1.4rem;"></i>
+      My Services GUI
+    </h1>
+    <div class="w-full max-w-[500px] my-5 flex flex-col gap-3">
+      <RouterLink v-for="r in routes" :to="r.path">
+        <Card>
+          <template #content>
+            <div class="flex justify-between items-center">
+              <h2 class="text-lg font-semibold">{{ r.title }}</h2>
+              <i class="pi pi-chevron-right" style="font-size: 1.2rem;"></i>
+            </div>
+          </template>
+        </Card>
+      </RouterLink>
+    </div>
+  </div>
 </template>
--- src/utils/casting.js
+++ src/utils/casting.js
@@ -1,5 +1,5 @@
 export const ShowDate = (iso) => {
   const date = new Date(iso)
-  const formated = date.toLocaleString()
+  const formated = date.toLocaleString(undefined, { hour12: false })
   return formated == 'Invalid Date' ? '-' : formated
 }
