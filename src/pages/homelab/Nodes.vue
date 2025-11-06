aeff8977544471ef006af11b4dfcbb62105707c3
Satrio Bagus <satrio.baguswbw@gmail.com>
2025-11-06 23:49:25 +0700

    ench: node & pod popup detail

--- src/pages/homelab/Nodes.vue
+++ src/pages/homelab/Nodes.vue
@@ -1,14 +1,21 @@
 <script setup>
 import CustomPaginator from '@/components/CustomPaginator.vue';
 import LastFetch from '@/components/LastFetch.vue';
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
 import { usePagination } from '@/composables/usePagination';
 import { useReqMetric } from '@/composables/useReqMetric';
+import { useDarkModeStore } from '@/stores/useDarkMode';
 import { changeUnit } from '@/utils/calc';
-import { Badge, Column, ColumnGroup, DataTable, Row } from 'primevue';
-import { computed, onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import { Badge, Button, Column, ColumnGroup, DataTable, Dialog, Row } from 'primevue';
+import { computed, onMounted, ref } from 'vue';
 const props = defineProps(['setLoading'])
 const { data: nodes, fetchData: fetchNodes } = useReqMetric()
+const { data: detail, fetchData: fetchDetail } = useReqMetric()
 const { limit, offset, limitOptions } = usePagination()
+const darkMode = useDarkModeStore()
+const { dmState } = storeToRefs(darkMode)
 document.title = "Nodes - Homelab"
 onMounted(() => {
   props.setLoading(true)
@@ -33,6 +40,15 @@ const totals = computed(() => {
   }
   return result
 })
+const isDetailOpen = ref(false)
+const GetDetail = (data) => {
+  detail.value = {}
+  props.setLoading(true)
+  isDetailOpen.value = true
+  const item = `node/${data.name}`
+  fetchDetail('detail', 'post', { name: item })
+    .finally(() => props.setLoading(false))
+}
 </script>
 <template>
   <div class="my-5 px-3 flex justify-between items-center">
@@ -69,12 +85,18 @@ const totals = computed(() => {
           <div class="text-sm">OS: <span class="font-bold">{{ d.data.os_image }}</span></div>
         </template>
       </Column>
+      <Column header="">
+        <template #body="d">
+          <Button v-tooltip.bottom="'Show Detail'" size="small" severity="info" icon="pi pi-eye" @click="GetDetail(d.data)"></Button>
+        </template>
+      </Column>
       <ColumnGroup type="footer">
         <Row>
           <Column footer="Total:" footer-style="text-align:right" />
           <Column :footer="totals.cpu" />
           <Column :footer="`${changeUnit(totals.ram, nodes?.data[0].memory_unit, 'Gi')} GiB`" />
-          <Column :footer="`${changeUnit(totals.storage, nodes?.data[0].storage_unit, 'Gi')} GiB`" :colspan="2" />
+          <Column :footer="`${changeUnit(totals.storage, nodes?.data[0].storage_unit,
+'Gi')} GiB`" :colspan="3" />
         </Row>
       </ColumnGroup>
       <template #footer>
@@ -83,4 +105,12 @@ const totals = computed(() => {
       </template>
     </DataTable>
   </div>
+  <Dialog v-model:visible="isDetailOpen" class="w-full! md:w-300! pb-5" modal :header="detail?.title ?
+    detail.title : 'Loading Detail...'">
+    <VueJsonPretty v-if="detail?.data" :data="detail.data" :show-line-number="true" :theme="dmState ? 'dark' : 'light'"
+      :show-icon="true" />
+    <div v-else class="flex justify-center">
+      <span class="pi pi-spinner pi-spin"></span>
+    </div>
+  </Dialog>
 </template>
--- src/pages/homelab/Pods.vue
+++ src/pages/homelab/Pods.vue
@@ -1,21 +1,27 @@
 <script setup>
 import CustomPaginator from '@/components/CustomPaginator.vue';
 import LastFetch from '@/components/LastFetch.vue';
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
 import { usePagination } from '@/composables/usePagination';
 import { useReqMetric } from '@/composables/useReqMetric';
-import { Card, Column, DataTable } from 'primevue';
-import { computed, onMounted } from 'vue';
+import { Button, Card, Column, DataTable, Dialog } from 'primevue';
+import { computed, onMounted, ref } from 'vue';
+import { useDarkModeStore } from '@/stores/useDarkMode';
+import { storeToRefs } from 'pinia';
 document.title = "Pods - Homelab"
 const { data: kinds, fetchData: fetchKinds } = useReqMetric()
 const { data: pods, fetchData: fetchPods } = useReqMetric()
+const { data: detail, fetchData: fetchDetail } = useReqMetric()
 const { limit, offset, limitOptions } = usePagination()
 const props = defineProps(['setLoading'])
+const darkMode = useDarkModeStore()
+const { dmState } = storeToRefs(darkMode)
 onMounted(() => {
   props.setLoading(true)
   init()
 })
-
-const init = async () => {
+const init = () => {
   Promise.allSettled([
     fetchPods('pods'),
     fetchKinds('kinds')
@@ -27,6 +33,16 @@ const tableData = computed(() => {
   const paginate = data.slice(offset.value, limit.value + offset.value)
   return paginate
 })
+
+const isDetailOpen = ref(false)
+const GetDetail = (data) => {
+  detail.value = {}
+  props.setLoading(true)
+  isDetailOpen.value = true
+  const item = `${data.kind.toLowerCase()}/${data.name} -n ${data.namespace}`
+  fetchDetail('detail', 'post', { name: item })
+    .finally(() => props.setLoading(false))
+}
 </script>
 <template>
   <div class="my-5 px-3 flex justify-between items-center">
@@ -66,10 +82,23 @@ const tableData = computed(() => {
           </ul>
         </template>
       </Column>
+      <Column header="">
+        <template #body="d">
+          <Button v-tooltip.bottom="'Show Detail'" size="small" severity="info" icon="pi pi-eye" @click="GetDetail(d.data)"></Button>
+        </template>
+      </Column>
       <template #footer>
         <CustomPaginator v-model:rows="limit" v-model:first="offset" :totalRecords="pods?.data.length"
           :rowsPerPageOptions="limitOptions" />
       </template>
     </DataTable>
   </div>
+  <Dialog v-model:visible="isDetailOpen" class="w-full! md:w-300! pb-5" modal :header="detail?.title ?
+    detail.title : 'Loading Detail...'">
+    <VueJsonPretty v-if="detail?.data" :data="detail.data" :show-line-number="true" :theme="dmState ? 'dark' : 'light'"
+      :show-icon="true" />
+    <div v-else class="flex justify-center">
+      <span class="pi pi-spinner pi-spin"></span>
+    </div>
+  </Dialog>
 </template>
